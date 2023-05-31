d9201bf197339c4fd46db6f7737f9658a5175845
guo-zhaohao <guozhh@mail2.sysu.edu.cn>
2023-05-31 23:03:20 +0800

    test

--- backend/test/test.go
+++ backend/test/test.go
@@ -8,8 +8,11 @@ import (
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("../前端/html/*")
-	router.GET("/", func(context *gin.Context) {
+	router.GET("", func(context *gin.Context) {
 		context.HTML(http.StatusOK, "首页.html", gin.H{})
+	})
+	router.GET("/体育.html", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "体育.html", gin.H{})
 	})
 	router.Run(":80")
 }
