85c47df905302ab4fc489b5884122692b5d6cc8d
guo-zhaohao <guozhh@mail2.sysu.edu.cn>
2023-06-05 16:56:19 +0800

    version2.0

---
+++ 前端/element_ui/src/views/Health.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <span class="top">
+            <h2>新闻论坛</h2>
+            <!-- vue输入用户名 -->
+            <div class="user">用户名：</div>
+            <div class="nav">
+                <ul>
+                    <li><a href="/home">首页</a></li>
+                    <li><a href="/pe">体育</a></li>
+                    <li><a href="/science">科技</a></li>
+                    <li><a href="/entertainment">娱乐</a></li>
+                    <li><a href="/game">游戏</a></li>
+                    <li><a href="/health" style="font-weight: 800;">健康</a></li>
+                    <li><a href="/other">其他</a></li>
+                </ul>
+            </div>
+        </span>
+    </div>
+</template>
+<style>
+@import '../assets/style/健康style.css'
+</style>
