e3da749d42c30b39fd7a53c7b7e9c3a6787575e1
18928603506 <11616605+yao-longji@user.noreply.gitee.com>
2023-06-09 11:10:23 +0800

    新闻页api请求修改

--- 前端/element_ui/src/components/page/articlepage.vue
+++ 前端/element_ui/src/components/page/articlepage.vue
@@ -1,32 +1,38 @@
 <template>
   <div class="news">
-    <h1>{{ article.title }}</h1>
-    <p>{{ article.date }} {{ article.src }}</p>
+    <h1>{{ article[0].title }}</h1>
+    <p>{{ article[0].time }} {{ article[0].src }}</p>
     <div class="picture">
-      <img :src="article.pic" alt="image" style="text-align:center;">
+      <img :src="article[0].pic" alt="image" style="text-align:center;">
     </div>
-    <div class="content" v-html="article.content"></div>
+    <div class="content" v-html="article[0].content"></div>
   </div>
 </template>
   
 <script>
-// import { getNewsList } from '@/api/getNewsList.js';
+ import { getNewsList } from '@/api/getNewsList.js';
 export default {
   data() {
     return {
-      article: {
-        title: '官方：贝林厄姆加盟皇马 转会费总价1.3亿欧元',
-        date: '2023-06-08',
-        src: '新浪体育讯',
-        pic: 'https://n.sinaimg.cn/sports/transform/361/w649h512/20230608/22b2-18b5cc1aeb65892bc3319b622b0e42f7.png',
-        content: '<p class=\"art_p\">6月8日,多特蒙德官方宣布,球队中场贝林厄姆正式转会皇马,转会费总价为1.3亿,分为1亿固定转会费,以及3000万的浮动条款。',
-      },
+      // article: {
+      //   title: '官方：贝林厄姆加盟皇马 转会费总价1.3亿欧元',
+      //   time: '2023-06-08',
+      //   src: '新浪体育讯',
+      //   pic: 'https://n.sinaimg.cn/sports/transform/361/w649h512/20230608/22b2-18b5cc1aeb65892bc3319b622b0e42f7.png',
+      //   content: '<p class=\"art_p\">6月8日,多特蒙德官方宣布,球队中场贝林厄姆正式转会皇马,转会费总价为1.3亿,分为1亿固定转会费,以及3000万的浮动条款。',
+      // },
+      article: {}
     };
   },
-  // created: async function () {
-  //     this.articlelist = await getNewsList(10, '体育')
-  //     console.log(this.articlelist)
-  // }
+  computed: {
+    async fetchNews() {
+      return await getNewsList(1, '体育');
+    },
+  },
+  async created() {
+    this.article = await this.fetchNews;
+    console.log(this.article);
+  },
 };
 </script>
   
