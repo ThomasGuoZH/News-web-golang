b765b6ab99656be3275ea17ef7e3b3e5dd7f4e54
guo-zhaohao <guozhh@mail2.sysu.edu.cn>
2023-06-12 16:13:36 +0800

    注册功能

--- backend/router/router.go
+++ backend/router/router.go
@@ -1,37 +1,70 @@
-package main
+package router
 
 import (
+	"backend/control"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 // 用户路由组
 func UserRouterInit(r *gin.RouterGroup) {
-	/*userManager := r.Group("/users")
+	userManager := r.Group("/user")
 	{
-		userManager.GET("/register", RegisterHandler())
-		userManager.GET("/login", LoginHandler())
-
-	}*/
+		userManager.POST("/register", control.RegisterHandler)
+		userManager.POST("/login", control.LoginHandler)
+		//userManager.GET("/refresh_token", control.RefreshTokenHandler)
+		//userManager.Use(control.JWTAuthMiddleware)
+		//{
+		//	userManager.GET("/userinfo", control.UserInfoHandler)
+		//}
+	}
 }
 
 // 新闻路由组
-func ArticleRouterInit(r *gin.RouterGroup) {
-	articleManager := r.Group("/users")
+/*func NewsRouterInit(r *gin.RouterGroup) {
+	newsManager := r.Group("/article")
 	{
-		articleManager.GET("/register")
-		articleManager.GET("/login")
-		//
+		newsManager.POST("/store",control.StoreHandler)
+		newsManager.GET("/get_news",control.GetNewsHandler)
 	}
-}
+}*/
 
-func main() {
+// 评论路由组
+//func CommentRouterInit(r *gin.RouterGroup) {
+//	commentManager := r.Group("/comment")
+//	{
+//		commentManager.GET("/comment_list", control.CommentListHandler)
+//		commentManager.Use(control.JWTAuthMiddleware)
+//		{
+//			commentManager.POST("/comment", control.UserCommentHandler)
+//			commentManager.GET("/get_comment", control.GetCommentHandler)
+//		}
+//	}
+//}
+
+//点赞路由组
+/*func LikeRouterInit(r *gin.RouterGroup) {
+	likeManager := r.Group("/like")
+	{
+		likeManager.Use(control.JWTAuthMiddleware){
+			likeManager.POST("/like", control.LikeHandler)
+			likeManager.GET("/get_like", control.GetLikeHandler)
+			likeManager.GET("/like_count", control.LikeCountHandler)
+	 	}
+	}
+}*/
+
+func SetupRouter() *gin.Engine {
 	router := gin.Default()
+	// 添加CORS中间件
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:8080", "http://localhost:8081", "http://localhost:8082", "http://localhost:8083",
+		"http://localhost:8084", "http://localhost:8085"} // 允许访问的域名
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"} // 允许的HTTP方法
+	router.Use(cors.New(config))
 	api := router.Group("api")
 	UserRouterInit(api)
-	ArticleRouterInit(api)
-
-	err := router.Run(":80")
-	if err != nil {
-		return
-	}
+	//NewsRouterInit(api)
+	//CommentRouterInit(api)
+	return router
 }
