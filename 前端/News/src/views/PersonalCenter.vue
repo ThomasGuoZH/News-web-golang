5705926553d1764b940b5e145670cfd860109eea
18928603506 <11616605+yao-longji@user.noreply.gitee.com>
2023-06-22 21:15:31 +0800

    changepassword的返回键

--- 前端/News/src/views/PersonalCenter.vue
+++ 前端/News/src/views/PersonalCenter.vue
@@ -24,15 +24,6 @@ import myMessage from '../components/user/myMessage.vue';
 export default {
     data() {
     return {
-      items: [
-        { name: '首页', url: '/home' },
-        { name: '时政', url: '/politics' },
-        { name: '科技', url: '/science' },
-        { name: '娱乐', url: '/entertainment' },
-        { name: '体育', url: '/sports' },
-        { name: '军事', url: '/military' },
-        { name: '教育', url: '/education' },
-        { name: '个人中心',url: '/'}],
       channel: '',
       pathFromUrl: '',
     }
@@ -41,41 +32,13 @@ export default {
         personal_nav,
         myMessage
     },
-    computed: {
-    getchannel() {
-      if (this.channel === '' || this.channel === 'home') {
-        return '首页';
-      } else if (this.channel === 'education') {
-        return '教育';
-      } else if (this.channel === 'science') {
-        return '科技';
-      } else if (this.channel === 'politics') {
-        return '时政';
-      } else if (this.channel === 'sports') {
-        return '体育';
-      } else if (this.channel === 'military') {
-        return '军事';
-      } else if (this.channel === 'entertainment') {
-        return '娱乐';
-      }
-    },
-    channelname() {
-      return this.getchannel;
-    }
-  },
-  beforeRouteEnter(to, from, next) {
-    next(vm => {
-      vm.pathFromUrl = from.fullPath;
-      vm.channel = to.params.channel;
-    })
-  },
     methods: {
     routeBack() {
-      this.$router.push({ path: this.pathFromUrl })
+      this.$router.push({ path: '/' })
       window.close();
     }
   },
-}
+ }
 </script>
 
 <style scoped>
