e1931b43dfeaecf0bb5c26b5c6c137f5040b40cf
guo-zhaohao <guozhh@mail2.sysu.edu.cn>
2023-07-05 16:46:07 +0800

    修改

--- backend/control/comment.go
+++ backend/control/comment.go
@@ -81,3 +81,34 @@ func UserChildCommentHandler(c *gin.Context) {
 		"time":    newChild.Model.CreatedAt.String()[:19],
 	}, "评论成功")
 }
+
+// 点赞处理
+func LikeHandler(c *gin.Context) {
+	var like *models.Likes
+	if err := c.ShouldBindJSON(&like); err != nil {
+		response.Fail(c, nil, "点赞失败")
+		return
+	}
+	var findComment models.Comment
+	fmt.Println(like.CommentId)
+	commentId, _ := strconv.Atoi(like.CommentId)
+	result := mysql.DB.Where("ID=?", commentId).First(&findComment)
+	if result.Error != nil {
+		response.Fail(c, nil, "没有找到该评论")
+	} else if result.Error == nil {
+		var likedByUser models.Likes
+		result = mysql.DB.Where("comment_id = ? AND liker = ?", commentId, like.Liker).First(&likedByUser)
+		if result.RowsAffected > 0 {
+			response.Fail(c, nil, "你已经点赞过该评论")
+			return
+		}
+		mysql.DB.Create(&like)
+		fmt.Println(findComment)
+		findComment.Likes++
+		mysql.DB.Save(&findComment)
+		response.Success(c, gin.H{
+			"likes": findComment.Likes,
+			"time":  findComment.Model.CreatedAt.String()[:19],
+		}, "点赞成功")
+	}
+}
