f34818c23db5e14f55298cc9f830bd4962f36280
18928603506 <11616605+yao-longji@user.noreply.gitee.com>
2023-07-06 09:29:55 +0800

    小改

--- 前端/News/vue.config.js
+++ 前端/News/vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     //以上的ip和端口是我们本机的;下面为需要跨域的
     proxy: { //配置跨域
       '/api': {
-        target: 'https://api.jisuapi.com/news/get?appkey=36f96971fb8e5a8e&start=0', //填写请求的目标地址
+        target: 'https://api.jisuapi.com/news/get?appkey=cc5e3b31d2129d83&start=40', //填写请求的目标地址
         changOrigin: true, //允许跨域
         pathRewrite: {
           '^/api': 'newsapi' //请求的时候使用这个api就可以
@@ -24,4 +24,5 @@ module.exports = {
 // faec4ccb9f663492
 // 36f96971fb8e5a8e
 // 37c8518088eabae4
+// 71f1655f9b3b89e4
 
